a6ea0cfec060177b5b9d3a00eb4f35a8449d4968
Mickaelle Pazuelo <mickaelle.pazuelo@gmail.com>
2021-07-13 12:56:24 +0200

    continue project1

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let questionIndex = 0;
 let score = 0;
+let total = 2;
 
 const questions = [
   {
-    title: "My 1st question",
-    choices: ["choice1", "choice2", "choice3", "choice4"],
+    title: "What is the name of the manager of Central Perk?",
+    choices: ["A) Gary", "B) Gunther", "C) Jerry", "His name is never pronounced"],
     correctAnswer: 1,
   },
   {
-    title: "My 2nd question",
-    choices: ["choice1", "choice2", "choice3", "choice4"],
-    correctAnswer: 3,
+    title: "In what year did the series Friends first start broadcasting in the Us?",
+    choices: ["1990", "1998", "1994", "2002"],
+    correctAnswer: 2,
   },
 ];
 
@@ -29,45 +30,40 @@ function displayQuestion(question) {
     // 2nd step
     // find a way to respond to a click when button gets clicked
     button.addEventListener("click", function () {
-
-        // ajouter une class au bouton cliqué
-
       // 3rd step
       // in button event handlers check if answer is correct
       if (question.correctAnswer === index) {
         console.log("it s the correct answer !");
-
+        // 4e step
+        // if answer is correct, score++ (OK), randomIndex++
         score++;
-        
       } else {
-        console.log("Bad news..it s wrong");
+        // console.log("Bad news..it s wrong");
+        // score = "";
       }
+      document.getElementById("score").innerHTML = score;
     });
   });
 
   let btn = document.getElementById("btn");
-  btn.addEventListener("click", function () {});
 
-  // 4e step
-  // if answer is correct, score++, randomIndex ++
-  
-  for (var i = 0; i < questions.length; i++) {
-    if (correctAnswer === questions[i]) {
-      score++;
-    } else {
-      console.log("you are wrong!");
-    }
-  }
-
-  console.log("you succeed" + score + "/" + questions.length);
 
   // 5e step
   // call display question with the new randomIndex
 }
 
+// Selectionner le bouton next
+// Appeler la fonction displayQuestion en oubliant d'increment
+// L'index de la question avant
+function NextQuestion() {
+  let next = document.querySelector(".btnNext");
+  next.addEventListener("click", function (event) {
+    questionIndex += 1;
+    displayQuestion(questions[questionIndex]);
+  });
+}
+NextQuestion();
+
 displayQuestion(questions[questionIndex]);
 
-
-// Selectionner le bouton next
-// Appeler la fonction displayQuestion en oubliant d'increment 
-// L'index de la question avant
+//Accumuler les scores de la Quest1 --> Quest2
